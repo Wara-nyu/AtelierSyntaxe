afc921fd4bdc974a9f630e32c17e6f510b909bc1
Wara-nyu <w.bounkeua@gmail.com>
2020-03-17 16:55:05 +0100

    ajout js

---
+++ ASyn.js
@@ -0,0 +1,16 @@
+/*console.log("Hello World!");
+
+function add(a,b){
+  console.log(a+b);
+}
+function presentation(age, nom, genre){
+  if(genre == "H"){
+    genre = "un homme"
+  }
+  if(genre == "F"){
+    genre = "une femme"
+  }
+  console.log("Bonjour, je suis " + genre + ", je m’appelle " + nom + " et j’ai " + age + " ans.");
+}*/
+
+
