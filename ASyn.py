d2580f3fb3cd6ee3f46ebb3d71aadbcf93bcb76c
Wara-nyu <w.bounkeua@gmail.com>
2020-03-18 15:08:41 +0100

    ajout d'un fonction romain presque fonctionnelle

--- ASyn.py
+++ ASyn.py
@@ -9,8 +9,31 @@ def presentation(age,prenom,genre):
         genre = "un homme"
     elif genre == "F" :
         genre = "une femme"
-    return "Bonjour, je suis " + genre +", je m'appelle " + prenom +" et j'ai " + age + " ans."
+    return "Bonjour, je suis {}, je m'appelle {} et j'ai {} ans.".format(genre, prenom, age)
 
 def romain (chiffre):
-#    for chiffre > 0 :
-     return  
+    resultat = "" 
+    while chiffre > 0 :
+        
+        if chiffre >= 1000 :
+            chiffre -= 1000
+            resultat += "M"
+        elif chiffre >= 500 :
+            chiffre -= 500
+            resultat += "D"
+        elif chiffre >= 100 :
+            chiffre -= 100
+            resultat += "C"
+        elif chiffre >= 50 :
+            chiffre -= 50
+            resultat += "L"
+        elif chiffre >= 10 :
+            chiffre -= 10
+            resultat += "X"
+        elif chiffre >= 5 :
+            chiffre -= 5
+            resultat += "V"
+        else :
+            chiffre -= 1
+            resultat += "I" 
+    return resultat 
--- testPourPython.py
+++ testPourPython.py
@@ -13,8 +13,31 @@ class testAsyn(unittest.TestCase):
         self.assertEqual("Bonjour, je suis un homme, je m'appelle Jean et j'ai 20 ans.", presentation("20","Jean","H"))
     
 class test_Romain(unittest.TestCase):
-    def test_renvoie_1(self):
+    def test_renvoie_I(self):
         self.assertEqual("I", romain(1))
 
-    #def
+    def test_renvoie_II(self):
+        self.assertEqual("II", romain(2))
+
+    def test_renvoie_V(self):
+       self.assertEqual("V", romain(5))
+
+    def test_renvoie_X(self):
+       self.assertEqual("X", romain(10))
+
+    def test_renvoie_L(self):
+       self.assertEqual("L", romain(50))
+
+    def test_renvoie_C(self):
+       self.assertEqual("C", romain(100))
+
+    def test_renvoie_D(self):
+       self.assertEqual("D", romain(500))
+
+    def test_renvoie_M(self):
+       self.assertEqual("M", romain(1000))
+
+    #def test_renvoie_IV(self):
+    #   self.assertEqual("IV", romain(4))
+
 unittest.main()
